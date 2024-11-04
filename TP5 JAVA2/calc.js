548330d5ea1378486e67f42a74afbd9d0776df99
Yanis <yanisleport@gmail.com>
2024-11-04 09:02:53 +0100

    js

---
+++ TP5 JAVA2/calc.js
@@ -0,0 +1,35 @@
+function addition(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)+Number(b);
+
+    t3.value = c;
+}
+
+function soustraction(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)-Number(b);
+
+    t3.value = c;
+}
+
+function division(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)/Number(b);
+
+    t3.value = c;
+}
+
+function multiplication(){
+    var a = document.getElementById("t1").value;
+    var b = document.getElementById("t2").value;
+    var c = Number(a)*Number(b);
+
+    t3.value = c;
+}
+
+function parite(){
+    
+}
