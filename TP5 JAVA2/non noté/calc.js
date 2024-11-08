e50244ffc1422ce4436d889e6102958d52f9d879
Yanis <yanisleport@gmail.com>
2024-11-08 21:40:55 +0100

    corrige mission 6 + tous le tp java

--- TP5 JAVA2/non noté/calc.js
+++ TP5 JAVA2/non noté/calc.js
@@ -30,6 +30,24 @@ function multiplication(){
     t3.value = c;
 }
 
+function permute(){
+    var v3 = "";
+    a = document.getElementById("t1").value;
+    b = document.getElementById("t2").value;
+    v3 = a
+    a = b
+    b = v3
+
+    t1.value = a;
+    t2.value = b;
+}
+
 function parite(){
-    
+    var v;
+    v = Number(document.getElementById("t3").value);
+    if(v%2==0)
+    t4.value = ("Pair")
+    else
+    t4.value = ("Impair")
+
 }
---
+++ TP5 JAVA2/noté/facture.js
@@ -0,0 +1,30 @@
+
+
+function resultat(){
+    var a = document.getElementById("t2").value;
+    var b = document.getElementById("t3").value;
+    var c = Number(a)*Number(b);
+
+    t4.value = c;
+
+
+
+    var d = document.getElementById("t6").value;
+    var e = document.getElementById("t7").value;
+    var f = Number(d)*Number(e);
+
+    t8.value = f;
+
+
+
+    var g = document.getElementById("t10").value;
+    var h = document.getElementById("t11").value;
+    var i = Number(g)*Number(h);
+
+    t12.value = i;
+
+    var t = c+f+i
+    t17.value = t
+}
+
+
