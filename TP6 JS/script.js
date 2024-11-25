4330c4fb3dc7bd6126c8e4e2b8b089c159628298
Yanis <yanisleport@gmail.com>
2024-11-25 10:21:13 +0100

    mission 8/2 surtout tp js

---
+++ TP6 JS/script.js
@@ -0,0 +1,68 @@
+function tableau(){
+    document.write("<table border=2px width=30%");
+    for(let i=0; i<=5; i++){
+        document.write("<tr> <td>*</td> <td>*</td> <td>*</td> </tr>")
+
+    }
+    document.write("</table>")
+}
+
+function cdc(){
+    var chaine=prompt ("Donnez un mot")
+    document.write (chaine.toUpperCase()+"<br>");
+    document.write (chaine.toLowerCase()+"<br>");
+    document.write ("La chiane contient" +chaine.length+ "caractere" + "<br>")
+    document.write ("la premiere lettre est " +chaine.substr(0,1)+ "<br>");
+
+}
+
+function tableau2(){
+    var X=prompt("Combien de ligne");
+    document.write("<table border=2px width=30%>")
+    for(let i=0; i<= X-1; i++){
+        document.write("<tr><td>"+(i+1)+"</td> <td>*</td> <td>*</td> </td>")
+    }
+    document.write("</table>")
+}
+
+function connect(){
+    var login=prompt ("Donnez votre Nom d'Utilisateur")
+    var password=prompt("Entre votre Mot de passe")
+    if (login=="admin" && password=="admin")
+        {document.write("Bienvenue :"+login);}
+    else (alert(document.write("Acces refuser")))
+}
+
+function connect2(){
+    window.location.href="authen.html"
+}
+
+function login(){
+    var login=document.getElementById("t1").value;
+    var pass=document.getElementById("t2").value;
+    if (login=="admin" && pass=="admin")
+        {window.location.href="yes.html"}
+    else {window.location.href="non.html"}
+}
+
+function connect3(){
+    i=0;
+    do{
+    var login=prompt("Donnez votre nom d'utilsateur0");
+    var password=prompt ("Entrez votre mot de passe");
+    if (login=="admin" && password=="admin")
+        {document.write("Bienvenu " +login);
+        break;}
+    else {alert ("Acces refuser");
+        i+=1;}
+    }
+    while(i<3)
+        if (i==3){alert("Délais dépassé")}
+    
+}
+
+function ajouter(){
+    var nom=document.getElementById("t1").value;
+    var age=document.getElementById("t2").value;
+    
+}
